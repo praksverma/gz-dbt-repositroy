f772fd64947e86a893d5a141a6f4661625682858
praksverma@gmail.com <praksverma@gmail.com>
2025-11-09 06:40:15 +0000

    tests

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,21 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1,
+        logcost,
+        CAST(ship_cost AS Float64) AS shipping_cost
+
+    from source
+)
+
+select * from renamed
