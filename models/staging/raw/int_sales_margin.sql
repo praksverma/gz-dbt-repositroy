662ac409935ef73e20625940112e7e3c8e4287fb
praksverma@gmail.com <praksverma@gmail.com>
2025-11-09 09:44:00 +0000

    sales margin

---
+++ models/staging/raw/int_sales_margin.sql
@@ -0,0 +1,22 @@
+with sales as(
+    select *
+    from {{ref('stg_raw__sales')}}
+),
+ product as(
+    select *
+    from {{ref('stg_raw__product')}}
+),
+joined as (
+    select
+    s.orders_id,
+    p.products_id,
+    s.revenue,
+    s.quantity,
+    p.purchase_price,
+    Round(quantity*purchase_price,2) AS purchase_cost,
+    Round(revenue-(quantity*purchase_price),2) as margin
+    From sales s
+   left join product p
+   on s.products_id=p.products_id
+)
+select * from joined
